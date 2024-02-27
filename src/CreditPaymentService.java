c2aa78d78f30ffb3cfbb5aa35e38b3e0a8e6c1d8
Katya <dolgova01011987@gmail.com>
2024-02-27 19:19:07 +0300

    Initiall commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,14 +1,7 @@
 public class CreditPaymentService {
     public double calculate(double sam, double years, double percent) {
         double p = percent / 100 / 12;
-        double a = years;
-        if (years == 1) {
-            a = 12;
-        } else if (years == 2) {
-            a = 24;
-        } else {
-            a = 36;
-        }
+        double a = years * 12;
         double b = sam;
 
         double payment = (p * Math.pow((1 + p), a) / ((Math.pow((1 + p), a) - 1))) * b;
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double years = 2;
+        double years = 1;
         double sam = 1_000_000;
         double payment = service.calculate(sam, years, 9.99);
 
