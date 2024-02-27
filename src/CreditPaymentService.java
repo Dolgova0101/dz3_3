cbdf7cbcc5470563199a5e6dba9fda1cd0f4f2c6
Katya <dolgova01011987@gmail.com>
2024-02-27 19:49:20 +0300

    Variables renamed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,10 @@
 public class CreditPaymentService {
-    public double calculate(double sam, double years, double percent) {
-        double p = percent / 100 / 12;
-        double a = years * 12;
-        double b = sam;
+    public double calculate(double credit, double years, double percent) {
+        double bid = percent / 100 / 12;
+        double months = years * 12;
+        double sam = credit;
 
-        double payment = (p * Math.pow((1 + p), a) / ((Math.pow((1 + p), a) - 1))) * b;
+        double payment = (bid * Math.pow((1 + bid), months) / ((Math.pow((1 + bid), months) - 1))) * sam;
         return payment;
 
 
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double years = 1;
-        double sam = 1_000_000;
-        double payment = service.calculate(sam, years, 9.99);
+        double credit = 1_000_000;
+        double payment = service.calculate(credit, years, 9.99);
 
 
         System.out.println("Ежемесячный платеж: " + Math.round(payment));
