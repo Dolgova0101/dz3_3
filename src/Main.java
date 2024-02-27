8c3e40457cb66d15d65518839ba18d1066869e31
Katya <dolgova01011987@gmail.com>
2024-02-27 18:22:59 +0300

    Initiall commit

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         double years = 2;
         double sam = 1_000_000;
-        double payment = service.calculate (sam,years,9.99);
+        double payment = service.calculate(sam, years, 9.99);
 
 
-        System.out.println("Ежемесячный платеж: "+Math.round(payment));
+        System.out.println("Ежемесячный платеж: " + Math.round(payment));
+    }
 }
-  }
